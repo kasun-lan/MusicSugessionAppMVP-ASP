947c6a485e11828798dfce3a06a9eda433979286
nadeeshan <nadeeshancooray@gmail.com>
2026-01-08 12:41:24 +0530

    Add project files.

---
+++ MusicSugessionAppMVP-ASP/Controllers/CrateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicSugessionAppMVP_ASP.Controllers
+{
+    public class CrateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Delivery()
+        {
+            return View();
+        }
+
+        public IActionResult HowItWorks()
+        {
+            return View();
+        }
+        public IActionResult Track2()
+        {
+            return View();
+        }
+    }
+}
