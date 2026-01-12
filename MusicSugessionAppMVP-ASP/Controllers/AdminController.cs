393794951f66de4d9a4d1f53c2edb12439be9b56
nadeeshan <nadeeshancooray@gmail.com>
2026-01-12 21:02:12 +0530

    dashboard added

---
+++ MusicSugessionAppMVP-ASP/Controllers/AdminController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicSugessionAppMVP_ASP.Models;
+using MusicSugessionAppMVP_ASP.Persistance;
+
+namespace MusicSugessionAppMVP_ASP.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly Persistence _db;
+
+        public AdminController(Persistence db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Dashboard()
+        {
+            var vm = new AdminDashboardViewModel
+            {
+                SessionsPerDay = _db.Sessions
+    .GroupBy(s => s.StartedAtUtc.Date)
+    .Select(g => new
+    {
+        Date = g.Key,
+        Count = g.Count()
+    })
+    .OrderBy(x => x.Date)
+    .AsEnumerable() // SQL ends here
+    .Select(x => new ChartPoint
+    {
+        Label = x.Date.ToString("yyyy-MM-dd"),
+        Value = x.Count
+    })
+    .ToList(),
+
+
+                SwipesPerDay = _db.SwipeEvents
+    .GroupBy(s => s.SwipedAtUtc.Date)
+    .Select(g => new
+    {
+        Date = g.Key,
+        Count = g.Count()
+    })
+    .OrderBy(x => x.Date)
+    .AsEnumerable()
+    .Select(x => new ChartPoint
+    {
+        Label = x.Date.ToString("yyyy-MM-dd"),
+        Value = x.Count
+    })
+    .ToList(),
+                RegistrationsPerDay = _db.Users
+    .GroupBy(u => u.RegisteredAtUtc.Date)
+    .Select(g => new
+    {
+        Date = g.Key,
+        Count = g.Count()
+    })
+    .OrderBy(x => x.Date)
+    .AsEnumerable()
+    .Select(x => new ChartPoint
+    {
+        Label = x.Date.ToString("yyyy-MM-dd"),
+        Value = x.Count
+    })
+    .ToList(),
+
+
+                PreferredStreamingPlatforms = _db.Users
+                    .GroupBy(u => u.PreferredStreamingPlatform.Name)
+                    .Select(g => new ChartPoint
+                    {
+                        Label = g.Key,
+                        Value = g.Count()
+                    })
+                    .ToList(),
+
+                DeviceTypeDistribution = _db.Sessions
+                    .GroupBy(s => s.DeviceType)
+                    .Select(g => new ChartPoint
+                    {
+                        Label = g.Key.ToString(),
+                        Value = g.Count()
+                    })
+                    .ToList(),
+
+                MostPopularGenres = _db.GenreExposures
+                    .GroupBy(g => g.Genre.Name)
+                    .OrderByDescending(g => g.Count())
+                    .Take(5)
+                    .Select(g => g.Key)
+                    .ToList(),
+
+                TotalSwipes = _db.SwipeEvents.Count(),
+
+                AverageSwipesPerSession = _db.SwipeEvents.Count() /
+                    (double)Math.Max(1, _db.Sessions.Count()),
+
+                MostRightSwipedTracks = _db.SwipeEvents
+    .Where(s => s.Direction == SwipeDirection.Like)
+    .GroupBy(s => new
+    {
+        TrackName = s.Track.Name,
+        ArtistName = s.Track.Artist.Name
+    })
+    .OrderByDescending(g => g.Count())
+    .Take(10)
+    .Select(g => new TrackSwipeDto
+    {
+        TrackName = g.Key.TrackName,
+        ArtistName = g.Key.ArtistName,
+        Likes = g.Count()
+    })
+    .ToList()
+
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ MusicSugessionAppMVP-ASP/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace MusicSugessionAppMVP_ASP.Models
+{
+    public class AdminDashboardViewModel
+    {
+        // Charts
+        public List<ChartPoint> SessionsPerDay { get; set; }
+        public List<ChartPoint> SwipesPerDay { get; set; }
+        public List<ChartPoint> RegistrationsPerDay { get; set; }
+
+        // Pie charts
+        public List<ChartPoint> PreferredStreamingPlatforms { get; set; }
+        public List<ChartPoint> DeviceTypeDistribution { get; set; }
+
+        // Lists / Numbers
+        public List<string> MostPopularGenres { get; set; }
+        public int TotalSwipes { get; set; }
+        public double AverageSwipesPerSession { get; set; }
+        public List<TrackSwipeDto> MostRightSwipedTracks { get; set; }
+    }
+
+    public class ChartPoint
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class TrackSwipeDto
+    {
+        public string TrackName { get; set; }
+        public string ArtistName { get; set; }
+        public int Likes { get; set; }
+    }
+}
